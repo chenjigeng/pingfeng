06c90be15bbf1f3812d21c8e709c4b9e5d05b49e
陈纪庚 <178854407@qq.com>
2017-11-19 00:00:19 +0800

    webpack配置修改

---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: './index.html',
+    })
+  ],
+  devtool: 'eval'
+};
