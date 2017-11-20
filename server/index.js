f34d5250ba5560de8c31567c07a7ddb36719ed79
陈纪庚 <178854407@qq.com>
2017-11-21 01:51:40 +0800

    引入connect-history-api-fallback

--- server/index.js
+++ server/index.js
@@ -4,38 +4,37 @@ var express = require('express');
 var app = express();
 
 app.use(require('morgan')('short'));
+app.use(require('connect-history-api-fallback')())
 
 // ************************************
 // This is the real meat of the example
 // ************************************
-(function() {
 
-  // Step 1: Create & configure a webpack compiler
-  var webpack = require('webpack');
-  var webpackConfig = require('../webpack.dev');
-  var compiler = webpack(webpackConfig);
+// Step 1: Create & configure a webpack compiler
+var webpack = require('webpack');
+var webpackConfig = require('../webpack.dev');
+var compiler = webpack(webpackConfig);
 
-  // Step 2: Attach the dev middleware to the compiler & the server
-  app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: true, 
-    publicPath: webpackConfig.output.publicPath,
-    reload: true,
-    stats: { colors: true },
-    quiet: true    
-  }));
-  var hotMiddleware = require("webpack-hot-middleware")(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-  });
+// Step 2: Attach the dev middleware to the compiler & the server
+app.use(require("webpack-dev-middleware")(compiler, {
+  noInfo: true, 
+  publicPath: webpackConfig.output.publicPath,
+  reload: true,
+  stats: { colors: true },
+  quiet: true    
+}));
+var hotMiddleware = require("webpack-hot-middleware")(compiler, {
+  log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+});
 
-  compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-      hotMiddleware.publish({ action: 'reload' })
-      cb()
-    })
+compiler.plugin('compilation', function (compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
   })
-  // Step 3: Attach the hot middleware to the compiler & the server
-  app.use(hotMiddleware);
-})();
+})
+// Step 3: Attach the hot middleware to the compiler & the server
+app.use(hotMiddleware);
 
 
 
