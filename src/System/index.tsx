1d718f29fc58d27b567bdd976554b39589dd82c2
陈纪庚 <178854407@qq.com>
2017-11-27 02:29:24 +0800

    导航栏优化

--- src/System/index.tsx
+++ src/System/index.tsx
@@ -1,7 +1,27 @@
-import * as React from 'react'
+import * as React from 'react';
+import "./system.less";
+import { Tabs } from 'antd';
 
+const TabPane = Tabs.TabPane;
 export class System extends React.Component<any, any> {
   render() {
-    return <div>Systs22sems2s</div>
+    return (
+      <div>
+        <Tabs
+          defaultActiveKey="1"
+          tabPosition="left"
+        >
+          <TabPane tab="系统配置" key="setting">
+            系统配置
+          </TabPane>
+          <TabPane tab="API管理" key="api">
+            API管理
+          </TabPane>
+          <TabPane tab="系统集成" key="integration">
+            系统集成
+          </TabPane>
+        </Tabs>
+      </div>
+    )
   }
 }
